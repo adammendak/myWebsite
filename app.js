124bcc98508df03c335298a48baad35af562c877
adammendak <adam.mendak@gmail.com>
2018-06-13 19:43:02 +0200

    working

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/**', (req,res)=> {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(function (err, req, res, next) {
+    res.json({"error" : err.name + ": " + err.message});
+});
+
+const port = process.env.port || 3000;
+
+app.listen(3000, () => {
+    console.log(`port listening on port ${port}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require("path");
+module.exports = {
+    entry:["whatwg-fetch", "./application/index.jsx"]
+    ,
+    output: { filename: "out.js", path: path.resolve(__dirname, "dist") },
+    devServer: {
+        inline: true,
+        contentBase: './',
+        port: 3001
+        },
+    mode: "development", 
+    watch: true,
+    module: {
+    rules: [{
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader"
+            ,
+            options: {
+                presets: [
+                "es2015", 
+                "stage-2", 
+                "react"]
+            }
+        }
+    },
+    {
+        test: /\.css$/,
+        use: [ 'style-loader'
+        , 'css-loader' ]
+      }]
+}
+}
