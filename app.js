775dd39a25467ea55daa445825a1c8abcb621387
MendakA <Adam.Mendak@psat.pl>
2018-06-14 08:11:11 +0200

    PORT

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(function (err, req, res, next) {
     res.json({"error" : err.name + ": " + err.message});
 });
 
-const port = process.env.$PORT || 3000;
-
-app.listen(port, "0.0.0.0", () => {
-    console.log(`port listening on port ${port}`);
+// const port = process.env.PORT || 3000;
+//for heroku
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`port listening on port ${PORT}`);
     });
