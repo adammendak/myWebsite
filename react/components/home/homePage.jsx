a6b93bfe58b84e43703c3ac358af9ebbd8ee2ebc
MendakA <Adam.Mendak@psat.pl>
2018-06-15 07:17:28 +0200

    added

--- react/components/home/homePage.jsx
+++ react/components/home/homePage.jsx
@@ -8,6 +8,7 @@ class HomePage extends React.Component {
                 <h1>Witaj!</h1>
                 <p>jak widac strona jest jeszcze w powijakach i bede tutaj wrzucal swoje projekty i informacje o mnie</p>
                 <p>strona jest napisana na reakcie i nodzie link do repozytorium <a href="https://github.com/adammendak/myWebsite">tutaj</a></p>
+                <p>strone tez wrzucilem i bede rozwijal rownoczesnie w chmurze na Heroku <a href ="https://adammendak.herokuapp.com/">tutaj</a></p>
             </div>
         )
     }
