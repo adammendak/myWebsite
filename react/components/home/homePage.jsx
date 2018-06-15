bf56fdb7eee8215a06d6cebda785858af27574aa
adammendak <adam.mendak@gmail.com>
2018-06-15 19:08:30 +0200

    more

--- react/components/home/homePage.jsx
+++ react/components/home/homePage.jsx
@@ -21,8 +21,8 @@ class HomePage extends React.Component {
                     <div id={"myPhoto"}>
                         <img src={"./images/adamzdjecie.jpg"} alt={"moje zdjecie"}/>
                     </div>
-                    <div id={"aboutMeParagraph"} className={["text-center", "ml-5"]}>
-                        <h1>Witaj na mojej stronie!</h1>
+                    <div id={"aboutMeParagraph"} className={"text-center", "ml-5"}>
+                        <h1 className={"mb-3"}>Witaj na mojej stronie!</h1>
                         <p>jak widac strona jest jeszcze w powijakach i bede tutaj wrzucal swoje projekty i informacje o mnie</p>
                         <p>strona jest napisana na reakcie i nodzie link do repozytorium <a href="https://github.com/adammendak/myWebsite">tutaj</a></p>
                         <p>strone tez wrzucilem i bede rozwijal rownoczesnie w chmurze na Heroku <a href ="https://adammendak.herokuapp.com/">tutaj</a></p>
