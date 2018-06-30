7ec0b7261cbe4a275b7d1ecf84e30dc1bf17dd68
adammendak <adam.mendak@gmail.com>
2018-06-30 15:10:04 +0200

    nowy projekt

--- react/components/projects/projectsPage.jsx
+++ react/components/projects/projectsPage.jsx
@@ -10,6 +10,10 @@ class ProjectsPage extends React.Component {
                         <a href="http://www.memo.adammendak.pl" target={"_new"}>Todo Serwis w nodzie napisany</a>
                         <p><a href="https://github.com/adammendak/MEAN_MemoService" target={"_new"}>link do repozytorium</a></p>
                     </li>
+                    <li>
+                        <a href="https://productmanagement-adammendak.herokuapp.com" target={"_new"}>Sklep internetowy</a>
+                        <p>na razie repozytorium produktow, moze sklep internetowy z tego bedzie, napisany w spring boocie i angularze</p>
+                    </li>
                 </ul>
 
             </div>
