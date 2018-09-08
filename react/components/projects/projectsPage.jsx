0b834e8b79a4b9f455a9e891f63f5c1a60e14845
adammendak <adam.mendak@gmail.com>
2018-09-08 17:14:38 +0200

    more projects

--- react/components/projects/projectsPage.jsx
+++ react/components/projects/projectsPage.jsx
@@ -7,6 +7,11 @@ class ProjectsPage extends React.Component {
                 <h1>strona o projektach</h1>
                 <ul>
                     <li>
+                        <a href="http://www.login.adammendak.pl"target={"_new"}>Autoryzacja JWT</a>
+                        <p>front w reakcie strzelajacy do serwera autentykacyjnego napisanego w Javie. Przyklad autentykacji
+                            za pomoca Json Web Tockenow.
+                        </p>
+                    <li>
                         <a href="http://www.memo.adammendak.pl" target={"_new"}>Todo Serwis w nodzie napisany</a>
                         <p><a href="https://github.com/adammendak/MEAN_MemoService" target={"_new"}>link do repozytorium</a></p>
                     </li>
